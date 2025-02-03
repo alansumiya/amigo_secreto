6a612e29884d0c5571f604641a2a9a5434b6d162
Alan Sumiya <alan.sumiya@gmail.com>
2025-02-03 15:40:21 -0300

    Criar função para agregar amigos

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,3 +1,19 @@
 let listaNomes = [];
 
 
+function adicionarAmigo(){
+    let nomes = document.getElementById('amigo').value;
+    if(nomes == ''){
+        alert('Por favor, insira um nome!');
+    }else{
+    listaNomes.push(nomes);
+    }
+    limparCampo(); 
+    
+}
+
+function limparCampo(){
+    nomes = document.getElementById('amigo');
+    nomes.value = '';
+    
+}
